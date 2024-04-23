e7ae354d9ae2a2792d5fdaffeef16ee616f9adbc
unknown <Administrador@DTI-JEFSISDTH.lim.upeu.edu.pe>
2024-04-23 12:40:10 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'art-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'carmi-art';
+}

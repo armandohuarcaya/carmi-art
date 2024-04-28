2eb800c95b6ffa505d76e3c610b09fba8d9bb06e
unknown <Administrador@DTI-JEFSISDTH.lim.upeu.edu.pe>
2024-04-28 17:29:46 -0500

    ventas

--- src/app/shared/services/pdf.service.ts
+++ src/app/shared/services/pdf.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import * as jspdf from 'jspdf';
-import html2canvas from 'html2canvas';
+// import * as jspdf from 'jspdf';
+// import html2canvas from 'html2canvas';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +9,13 @@ export class PdfService {
 
   constructor() { }
   generarPDF(elementoHTML: HTMLElement, nombreArchivo: string) {
-    html2canvas(elementoHTML).then((canvas) => {
-      const contenido = 'mmmm';
-      const pdf = new jspdf.jsPDF();
-      const ancho = pdf.internal.pageSize.getWidth();
-      const alto = pdf.internal.pageSize.getHeight();
-      pdf.addImage(contenido, 'PNG', 0, 0, ancho, alto);
-      pdf.save(nombreArchivo + '.pdf');
-    });
+    // html2canvas(elementoHTML).then((canvas) => {
+    //   const contenido = 'mmmm';
+    //   const pdf = new jspdf.jsPDF();
+    //   const ancho = pdf.internal.pageSize.getWidth();
+    //   const alto = pdf.internal.pageSize.getHeight();
+    //   pdf.addImage(contenido, 'PNG', 0, 0, ancho, alto);
+    //   pdf.save(nombreArchivo + '.pdf');
+    // });
   }
 }

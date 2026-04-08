ef2abbfdd31f8d979c4a071d499e7e0277ca0853
Alexander Llacho <allacho@pkiep.reniec.gob.pe>
2026-04-08 12:34:19 -0500

    add view code for orden de pedido

--- src/environments/vhost.ts
+++ src/environments/vhost.ts
@@ -2,11 +2,11 @@ export const API = {
     art: {
         local: 'http://localhost:3031',
         dev: 'https://ms-admin-un3m.onrender.com',
-        prod: 'https://ms-admin-un3m.onrender.com',
+        prod: 'http://150.136.7.111:3031',
     },
     auth: {
       local: 'http://localhost:3030/api/auth',
       dev: 'https://ms-auth-6tie.onrender.com/api/auth',
-      prod: 'https://ms-auth-6tie.onrender.com/api/auth',
+      prod: 'http://150.136.7.111:3030/api/auth',
   }
 };
